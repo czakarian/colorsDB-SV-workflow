9a52d4ef0a27cb509c0260d2c5ca6438adcf796a
Christina Zakarian <czaka@uw.edu>
2025-05-02 11:52:26 -0700

    removed threads argument // not needed

--- run_colors_workflow.sh
+++ run_colors_workflow.sh
@@ -4,14 +4,13 @@ set -eu -o pipefail
 
 
 usage() {
-    echo "Usage: run_colors_workflow.sh -i <input_vcf> -o <output_dir> [-t <threads>]"
+    echo "Usage: run_colors_workflow.sh -i <input_vcf> -o <output_dir> [-m <min_sv_length>]"
     echo
     echo "Required arguments:"
     echo "  -i <input_vcf>         Input VCF (should be .gz and have .tbi index)."
     echo "  -o <output_dir>        Output directory."
     echo
     echo "Optional arguments:"
-    echo "  -t <threads>          Number of threads to use (default: 4)."
     echo "  -m <min_sv_length>    Filter out SVs smaller than set threshold. (default: 15)."
 
 }
@@ -23,14 +22,12 @@ if [ "$#" -eq 0 ]; then
 fi
 
 # Default values
-export THREADS=4  
 export MIN_SV_LENGTH=15
 
-while getopts "i:o:t:m:h" FLAG; do
+while getopts "i:o:m:h" FLAG; do
     case ${FLAG} in
         i) INPUTFILE=${OPTARG};;
         o) OUTDIR=${OPTARG};;
-        t) THREADS=${OPTARG};;
         m) MIN_SV_LENGTH=${OPTARG};;
         h) 
             usage
@@ -46,7 +43,7 @@ done
 
 ### modifiable variables
 export OVERSIZED_THRESHOLD=100000
-export UW1KG_STRICTNESS="relaxed" ## strict or relaxed
+export UW1KG_STRICTNESS="relaxed" ## strict or relaxed VCF
 ###
 
 export COLORS_WORKFLOW_ROOT="$(dirname "$(readlink -f "${BASH_SOURCE}")")"
@@ -77,14 +74,14 @@ fi
 
 echo "Input file = ${INPUTFILE}"
 echo "Output directory = ${OUTDIR}"
-echo "Threads = ${THREADS}"
+echo "Minimum SV Length = ${MIN_SV_LENGTH}"
 echo "Starting variant count: " $(zcat ${INPUTFILE} | grep -v '^#' | wc -l)
 
 echo '==' $(date) '==' Preprocess input VCF to filter for PASS and chr1-22,X,Y,M variants
 ${BCFTOOLSCMD} bcftools view -f PASS -i "INFO/SVLEN>${MIN_SV_LENGTH} || INFO/SVLEN<-${MIN_SV_LENGTH}" \
 -r chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22,chrX,chrY,chrM \
 -Oz -o ${OUTDIR}/${PREFIX}.preprocessed.vcf.gz ${INPUTFILE}
-${BCFTOOLSCMD} bcftools index --threads ${THREADS} --tbi ${OUTDIR}/${PREFIX}.preprocessed.vcf.gz
+${BCFTOOLSCMD} bcftools index --tbi ${OUTDIR}/${PREFIX}.preprocessed.vcf.gz
 
 echo "Remaining variant count: " $(zcat ${OUTDIR}/${PREFIX}.preprocessed.vcf.gz | grep -v '^#' | wc -l)
 
@@ -99,7 +96,7 @@ python ${COLORS_WORKFLOW_ROOT}/annotate_with_truvari_output.py \
 -r COLORS
 
 ${BCFTOOLSCMD} bcftools sort ${OUTDIR}/debug/${PREFIX}.colorAnno.vcf -Oz -o ${OUTDIR}/debug/${PREFIX}.colorAnno.vcf.gz
-${BCFTOOLSCMD} bcftools index --threads ${THREADS} --tbi ${OUTDIR}/debug/${PREFIX}.colorAnno.vcf.gz
+${BCFTOOLSCMD} bcftools index --tbi ${OUTDIR}/debug/${PREFIX}.colorAnno.vcf.gz
 
 echo "Remaining variant count: " $(zcat ${OUTDIR}/debug/${PREFIX}.colorAnno.vcf.gz | grep -v '^#' | wc -l)
 
@@ -114,7 +111,7 @@ python ${COLORS_WORKFLOW_ROOT}/annotate_with_truvari_output.py \
 -r UW1KG
 
 ${BCFTOOLSCMD} bcftools sort ${OUTDIR}/debug/${PREFIX}.colorAnno.1000gAnno.vcf -Oz -o ${OUTDIR}/debug/${PREFIX}.colorAnno.1000gAnno.vcf.gz
-${BCFTOOLSCMD} bcftools index --threads ${THREADS} --tbi ${OUTDIR}/debug/${PREFIX}.colorAnno.1000gAnno.vcf.gz 
+${BCFTOOLSCMD} bcftools index --tbi ${OUTDIR}/debug/${PREFIX}.colorAnno.1000gAnno.vcf.gz 
 
 echo "Remaining variant count: " $(zcat ${OUTDIR}/debug/${PREFIX}.colorAnno.1000gAnno.vcf.gz  | grep -v '^#' | wc -l)
 
@@ -122,7 +119,7 @@ echo '==' $(date) '==' Oversized SV iteration - Run truvari merge + annotate ste
 echo '==' $(date) '==' "Pull oversized SVs from preprocessed VCF (SVLEN>${OVERSIZED_THRESHOLD})"
 ${BCFTOOLSCMD} bcftools view -i "(INFO/SVLEN > ${OVERSIZED_THRESHOLD} || INFO/SVLEN < -${OVERSIZED_THRESHOLD})" \
 -Oz -o ${OUTDIR}/debug/${PREFIX}.oversizedSVs.vcf.gz ${OUTDIR}/${PREFIX}.preprocessed.vcf.gz
-${BCFTOOLSCMD} bcftools index --threads ${THREADS} --tbi ${OUTDIR}/debug/${PREFIX}.oversizedSVs.vcf.gz
+${BCFTOOLSCMD} bcftools index --tbi ${OUTDIR}/debug/${PREFIX}.oversizedSVs.vcf.gz
 
 echo "Oversized SV count: " $(zcat ${OUTDIR}/debug/${PREFIX}.oversizedSVs.vcf.gz | grep -v '^#' | wc -l)
 
@@ -137,7 +134,7 @@ python ${COLORS_WORKFLOW_ROOT}/annotate_with_truvari_output.py \
 -r COLORS
 
 ${BCFTOOLSCMD} bcftools sort ${OUTDIR}/debug/${PREFIX}.colorAnno.1000gAnno.withColorsOversized.vcf -Oz -o ${OUTDIR}/debug/${PREFIX}.colorAnno.1000gAnno.withColorsOversized.vcf.gz
-${BCFTOOLSCMD} bcftools index --threads ${THREADS} --tbi ${OUTDIR}/debug/${PREFIX}.colorAnno.1000gAnno.withColorsOversized.vcf.gz
+${BCFTOOLSCMD} bcftools index --tbi ${OUTDIR}/debug/${PREFIX}.colorAnno.1000gAnno.withColorsOversized.vcf.gz
 
 echo '==' $(date) '==' OVERSIZED - Truvari bench with 1000g
 ${TRUVARICMD} truvari bench -b ${UWONTVCF} -c ${OUTDIR}/debug/${PREFIX}.colorAnno.1000gAnno.withColorsOversized.vcf.gz -f ${REF} -o ${OUTDIR}/debug/truvari_bench_1000g_oversized \
@@ -150,17 +147,17 @@ python ${COLORS_WORKFLOW_ROOT}/annotate_with_truvari_output.py \
 -r UW1KG
 
 ${BCFTOOLSCMD} bcftools sort ${OUTDIR}/debug/${PREFIX}.colorAnno.1000gAnno.withColorsOversized.with1000gOversized.vcf -Oz -o ${OUTDIR}/debug/${PREFIX}.colorAnno.1000gAnno.withColorsOversized.with1000gOversized.vcf.gz
-${BCFTOOLSCMD} bcftools index --threads ${THREADS} --tbi ${OUTDIR}/debug/${PREFIX}.colorAnno.1000gAnno.withColorsOversized.with1000gOversized.vcf.gz
+${BCFTOOLSCMD} bcftools index --tbi ${OUTDIR}/debug/${PREFIX}.colorAnno.1000gAnno.withColorsOversized.with1000gOversized.vcf.gz
 
 echo '==' $(date) '==' Add annotated oversized SVs to final VCF
 ${BCFTOOLSCMD} bcftools concat -a ${OUTDIR}/debug/${PREFIX}.colorAnno.1000gAnno.vcf.gz ${OUTDIR}/debug/${PREFIX}.colorAnno.1000gAnno.withColorsOversized.with1000gOversized.vcf.gz \
 -Oz -o ${OUTDIR}/debug/${PREFIX}.colorAnno.1000gAnno.combined.vcf.gz
-${BCFTOOLSCMD} bcftools index --threads ${THREADS} --tbi ${OUTDIR}/debug/${PREFIX}.colorAnno.1000gAnno.combined.vcf.gz
+${BCFTOOLSCMD} bcftools index --tbi ${OUTDIR}/debug/${PREFIX}.colorAnno.1000gAnno.combined.vcf.gz
 
 echo '==' $(date) '==' Add bcftools fill-tags for AC,AN,AF tags 
 ${BCFTOOLSCMD} bcftools +fill-tags ${OUTDIR}/debug/${PREFIX}.colorAnno.1000gAnno.combined.vcf.gz -- -t AC,AN,AF \
 | ${BCFTOOLSCMD} bcftools sort -Oz -o ${OUTDIR}/${PREFIX}.colorAnno.1000gAnno.final.vcf.gz
-${BCFTOOLSCMD} bcftools index --threads ${THREADS} --tbi ${OUTDIR}/${PREFIX}.colorAnno.1000gAnno.final.vcf.gz
+${BCFTOOLSCMD} bcftools index --tbi ${OUTDIR}/${PREFIX}.colorAnno.1000gAnno.final.vcf.gz
 
 echo '==' $(date) '==' Clean up temp files
 rm ${OUTDIR}/debug/${PREFIX}*.vcf
